308dccde1907f616a340fe8648ac2390fa98efa7
ipkalm <ipkalm@outlook.com>
2022-09-25 18:56:31 +0700

    add solution for helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+	if(count < 0) {
+		return 'ERROR';
+	}
 
+	let tmp = '';
+	for (let i = 0; i < count; i++) {
+		tmp += str;		
+	}
+	return tmp;
 };
 
 // Do not edit below this line
