aa34edb19c1b5787f627ddb407be2579901feff7
ipkalm <ipkalm@outlook.com>
2022-09-25 19:07:57 +0700

    add function; 1st test pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	const tmp = str.split('');
+	str = '';
+	for (let i = tmp.length - 1; i >= 0; i--) {
+		str += tmp[i];
+	}
 
+	return str;
 };
 
 // Do not edit below this line
