e7b94759c7228353dba90ad0c099913bc0b14585
ipkalm <ipkalm@outlook.com>
2022-09-25 19:39:11 +0700

    04 remove from array all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+	if (args.length === 0) {
+		return false;
+	}
 
+	for (const arg of args) {
+		for (const arr in array) {
+			if (arg === array[arr]) {
+				array.splice(arr, 1);
+			}
+		}
+	}
+
+	return array;
 };
 
 // Do not edit below this line
